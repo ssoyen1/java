a16e06b880ce7fefc5bc83e84fa4d1210925df49
lee soyeon <ssoyen1@naver.com>
2022-09-23 23:16:02 +0900

    this 키워드

--- JAVA/src/this_/Ex.java
+++ JAVA/src/this_/Ex.java
@@ -3,7 +3,11 @@ package this_;
 public class Ex {
 
 	public static void main(String[] args) {
-		/*
+		/*각종 변수들
+		 * https://doya-life.tistory.com/entry/%EA%B0%9C%EB%85%90%EC%9D%B4%ED%95%B4-%EB%A9%A4%EB%B2%84%EB%B3%80%EC%88%98%EC%9D%B8%EC%8A%A4%ED%84%B4%EC%8A%A4-%EB%B3%80%EC%88%98-%EC%A7%80%EC%97%AD%EB%B3%80%EC%88%98%EB%A1%9C%EC%BB%AC%EB%B3%80%EC%88%98-static%EB%B3%80%EC%88%98%ED%81%B4%EB%9E%98%EC%8A%A4%EB%B3%80%EC%88%98-%EC%B0%B8%EC%A1%B0%EB%B3%80%EC%88%98-%EB%A7%A4%EA%B0%9C%EB%B3%80%EC%88%98
+		 * 
+		 * 
+		 *
 		 * this 키워드
 		 * - 자신의 인스턴스 주소값을 저장하는 참조 변수
 		 * 	 => 개발자가 생성하는 것이 아니라 자바에 의해 자동으로 생성됨
@@ -60,7 +64,7 @@ public class Ex {
 			   public void setName(String name) {
 				      // 메서드 내에서 로컬변수와 클래스 내의 멤버변수의 이름이 동일할 경우
 				      // 메서드 내에서 변수 지정 시 로컬변수가 지정
-				      this.name = name; // 로컬변수 name 값을 다시 로컬변수 name에 저장하는 코드
+				      this.name = name; // 로컬변수 name 값을 다시 멤버변수 name에 저장하는 코드
 				      
 				      // 로컬변수와 멤버변수를 구별하기 위해서는 멤버변수명 앞에
 				      // 레퍼런스 this를 사용하여 해당 인스턴스에 접근하는 코드로 사용해야함
